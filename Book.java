1806dc6bce4e50c3a3826e69ef9f1c27eb99bd1b
Duc Phuc <166194999+Hinokamika@users.noreply.github.com>
2024-12-19 09:22:04 +0700

    .

---
+++ Book.java
@@ -0,0 +1,61 @@
+public class Book {
+    private int id;
+    private String name;
+    private String author;
+    private int Stockquantity;
+    private double price;
+
+    public Book(int id, String name, String author, int Stockquantity, double price) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.Stockquantity = Stockquantity;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getQuantity() {
+        return Stockquantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setQuantity(int Stockquantity) {
+        this.Stockquantity = Stockquantity;
+    }
+
+    public boolean inStock(int quantity) {
+        if (Stockquantity >= quantity) {
+            Stockquantity -= quantity;
+            return true;
+        }else {
+            System.out.println("Sorry, the book is out of stock");
+            return false;
+        }
+        
+    }
+
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", Stockquantity=" + Stockquantity +
+                ", price=" + price +
+                '}';
+    }
+}
+
